b378fdd0e3e566063c9bd664074eb16790f0b7ce
oonsosin <namakusansiro@gmail.com>
2021-03-31 18:31:37 +0700

    Update sumber.sh

--- sumber.sh
+++ sumber.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=ethash-asia-eth.2miners.com:2020
+POOL=asia-eth.2miners.com:2020
 WALLET=0xc6601e3308cfe311faa2236b9578ae3cfad2c82a
 WORKER=SHOLAT
 
 cd "$(dirname "$0")"
 
-chmod +x ./rezeki && sudo ./rezeki -a ergo -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./rezeki && sudo ./rezeki -a ethash -o $POOL -u $WALLET.$WORKER $@
